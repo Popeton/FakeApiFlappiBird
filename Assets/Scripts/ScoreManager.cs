f47d1e88fbc34d72aef227620d18fc5feb5f0bcc
Abraham Daguer <abrahamg1235@gmail.com>
2022-09-25 12:21:11 -0500

    fake api

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using TMPro;
+
+
+
+public class ScoreManager : MonoBehaviour
+{
+    public GameObject scoreItems;
+    
+    private void OnEnable()
+    {
+        HTTPManager.OnScoresGet += ShowScores;
+    }
+
+    private void OnDisable()
+    {
+        HTTPManager.OnScoresGet -= ShowScores;
+    }
+
+    void ShowScores(ScoresData data)
+    {
+
+        transform.GetChild(0).gameObject.SetActive(true);
+
+        List<UserData> myScores = data.usuarios.ToList();
+
+        for (int i = 0; i < scoreItems.transform.childCount; i++)
+        {
+            if(i< myScores.Count)
+            {
+                scoreItems.transform.GetChild(i).GetComponent<TMP_Text>().text = string.Format("{0} {1} {2}",(i+1),myScores[i].username, myScores[i].score);
+            }
+            else
+            {
+                scoreItems.transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+       
+    }
+
+  
+}
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# FakeApiFlappiBird
- 
+# flappy-bird
